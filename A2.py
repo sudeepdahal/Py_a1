fca6c6fe370fc8551d9eecf93238b6e86f3e36d6
sudeep <sudeep.dahal05@gmail.com>
2019-09-15 14:46:03 +1000

    validation successful

--- A2.py
+++ A2.py
@@ -10,51 +10,23 @@ def main():
 	del_charge = 0.0
 	purchase_total = 0.0
 	n = 0
-	'''try:
-		question1 = int(input("Enter a number: "))
-	except ValueError:
-		print("That's not a number!")
-	else:
-		print("Congratulations - you followed the instructions")
-	question1 = input("Enter a number: ")
-	if question1.isdigit():
-		question1= int(question1)
-		print('adfadfasdfadsf')
-	else:
-		 print("Not a number")
-		 
-		 
-          purchase_total_type = None
-          while type(purchase_total_type) is not float:
-              try:
-                  purchase_total = input('\nPlease enter purchase total:')
-              except ValueError:
-                  print("%s is not float input.\n" % purchase_total_type)
-
-'''
 	#Repeating the delivery calculator if user enters y or Y
 	while (another_purchase == 'y' or another_purchase == 'Y'):
 		purchase_total = None
+		#Validating to get float value
 		while type(purchase_total) is not float:
 			try:
 				purchase_total = float(input('\nPlease enter purchase total:'))
-				#purchase_total_type = type(purchase_total)
 			except ValueError:
-				print("%s is not float input.Please Input Float value\n" % purchase_total)
-		
-		#checking for purchase total is above 150 or not
+				print("Invalid input.Please Input Float value")
+		#checking for purchase total is above 150 or not and returns boolean value
 		purchaseTotal = checkPurchaseTotal(purchase_total)
 		if purchaseTotal:
-			n = int(input('Please enter number of the items:'))
-			#Checking if number of items enterd user are negative or 0
-			while (n <= 0):
-				print('Invalid Number of Items');
-				n = int(input('Please enter number of the items:'))
-			del_day = int(input('Please enter delivery day ([1] for 1st day and [2] for 2nd day:'))
-			#Checking if delivery day are besides 1 or 2
-			while not (del_day == 1 or del_day == 2):
-				print('Invalid Number of Delivery Days');
-				del_day = int(input('Please enter delivery day ([1] for 1st day and [2] for 2nd day:'))
+			#call function to get validate input and store in n variable
+			n = getValidatedNoOfDays()
+			#call function to get validate input and store in del_day variable
+			del_day = getValidatedDelDay()
+			
 			#Calling the calculating function
 			del_charge,total_charges = calCharges(n,del_day,purchase_total)
 			#Display of delivery charges and total charges as floating-point numbers with 2 decimal places
@@ -65,9 +37,8 @@ def main():
 			print('ERR: Sorry, purchase total need to be above $150.')
 		#Asking if users wants to use the delivery calculator again
 		another_purchase = input('Do you want to calculate delivery charges for another purchase? (y/n)')
-	print('Thanks for using the delivery charges Calculator!\nSee you again!\n')
-	print('Total number of purchase made: ',number_of_the_purchase)
-	print('Total cost of Delivery made: ',format(cost_of_deliveries,'.2f'))
+	#Printing number of delivery and total cost of delivery
+	endingProgramResult()
 def designCode():
 	for x in range(50):
 		print('-',end='')
@@ -79,7 +50,38 @@ def checkPurchaseTotal(purchase_total):
 		return True
 	else:
 		return False
-	print('aaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+def getValidatedNoOfDays():
+	#check if input is integer or not
+	n = None
+	#Validating to get positive int value
+	while True:
+		try:
+			n = int(input('Please enter number of the items:'))
+		except ValueError:
+			print("Invalid input.Please Input positive Integer value")
+			continue
+		else:
+			if (n<=0):
+				print("Invalid input.Please Input positive Integer value")
+				continue
+			else:
+				return n
+				break
+def getValidatedDelDay():
+	#check if input is integer or not and is 1 or 2
+	del_day = None
+	while True:
+		try:
+			del_day = int(input('Please enter delivery day ([1] for 1st day and [2] for 2nd day:'))
+		except ValueError:
+			print("Invalid input")
+			continue
+		else:
+			if (del_day == 1 or del_day == 2):
+				return del_day
+			else:
+				print("Invalid input")
+				continue
 def calCharges(n,del_day,purchase_total):
 	#calculation of delivery charges accordingly 
 	if n <= 5 and del_day == 1:
@@ -94,9 +96,13 @@ def calCharges(n,del_day,purchase_total):
 		#If non of the case behave accordingly exiting the program
 		print('Something went wrong')
 		exit()
-				
 	global number_of_the_purchase,cost_of_deliveries
 	number_of_the_purchase += 1
 	cost_of_deliveries += del_charge
 	return del_charge,purchase_total+del_charge
+def endingProgramResult():
+	print('Thanks for using the delivery charges Calculator!\nSee you again!\n')
+	print('Total number of purchase made: ',number_of_the_purchase)
+	print('Total cost of Delivery made: ',format(cost_of_deliveries,'.2f'))
+
 main()
